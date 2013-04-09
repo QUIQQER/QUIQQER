567c9e5757cf19e6b3257120c482bdb5008b5c3a
Henning Leutz <leutz@pcsg.de>
2013-04-09 08:00:27 +0200

    php doc

--- lib/interface/projects/media/File.php
+++ lib/interface/projects/media/File.php
@@ -19,99 +19,99 @@ interface Interface_Projects_Media_File
      * Return the file media ID
      * @return Integer
      */
-	public function getId();
+    public function getId();
 
-	/**
-	 * Return the Parent of the file
-	 * @return Projects_Media_Item
-	 */
-	public function getParent();
+    /**
+     * Return the Parent of the file
+     * @return Projects_Media_Item
+     */
+    public function getParent();
 
-	/**
-	 * Return the parent id
-	 * @return Integer
-	 */
-	public function getParentId();
+    /**
+     * Return the parent id
+     * @return Integer
+     */
+    public function getParentId();
 
-	/**
-	 * return all parent ids from the file
-	 * @return array
-	 */
-	public function getParentIds();
+    /**
+     * return all parent ids from the file
+     * @return array
+     */
+    public function getParentIds();
 
-	/**
-	 * Return the path from the file
-	 * @return String
-	 */
-	public function getPath();
+    /**
+     * Return the path from the file
+     * @return String
+     */
+    public function getPath();
 
-	/**
-	 * Return what type is the file
-	 * @return String Projects_Media_Image|Projects_Media_Folder|Projects_Media_File
-	 */
-	public function getType();
+    /**
+     * Return what type is the file
+     * @return String Projects_Media_Image|Projects_Media_Folder|Projects_Media_File
+     */
+    public function getType();
 
-	/**
-	 * Return the URL of the File, relative to the host
-	 * @return String
-	 */
-	public function getUrl();
+    /**
+     * Return the URL of the File, relative to the host
+     * @return String
+     */
+    public function getUrl();
 
-	/**
-	 * Activate the file
-	 * @throws QException
-	 */
-	public function activate();
+    /**
+     * Activate the file
+     * @throws QException
+     */
+    public function activate();
 
-	/**
-	 * Deactivate the file
-	 * @throws QException
-	 */
-	public function deactivate();
+    /**
+     * Deactivate the file
+     * @throws QException
+     */
+    public function deactivate();
 
-	/**
-	 * Delete the file, file is in trash
-	 * @throws QException
-	 */
-	public function delete();
+    /**
+     * Delete the file, file is in trash
+     * @throws QException
+     */
+    public function delete();
 
-	/**
-	 * Save the file with all its attributes to the Database
-	 * @throws QException
-	 */
-	public function save();
+    /**
+     * Save the file with all its attributes to the Database
+     * @throws QException
+     */
+    public function save();
 
-	/**
-	 * Renames the file
-	 *
-	 * @param String $newname - new name of the file
-	 * @throws QException
-	 */
-	public function rename($newname);
+    /**
+     * Renames the file
+     *
+     * @param String $newname - new name of the file
+     * @throws QException
+     */
+    public function rename($newname);
 
-	/**
-	 * create the file cache
-	 * @throws QException
-	 */
-	public function createCache();
+    /**
+     * create the file cache
+     * @throws QException
+     */
+    public function createCache();
 
-	/**
-	 * delete the file cache
-	 * @throws QException
-	 */
-	public function deleteCache();
+    /**
+     * delete the file cache
+     * @throws QException
+     */
+    public function deleteCache();
 
     /**
      * move the item to another folder
-     * @param Integer $folderid
+     * @param Projects_Media_Folder $Folder
      */
-	public function moveTo(Projects_Media_Folder $Folder);
+    public function moveTo(Projects_Media_Folder $Folder);
 
     /**
      * copy the item to another folder
-     * @param Integer $folderid
+     * @param Projects_Media_Folder $Folder
      */
-	public function copyTo(Projects_Media_Folder $Folder);
+    public function copyTo(Projects_Media_Folder $Folder);
 }
 
 
