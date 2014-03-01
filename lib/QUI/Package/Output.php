dd88cd04d815f716d22d0874f8c30fe9f85f53f9
Henning Leutz <leutz@pcsg.de>
2014-03-01 12:06:09 +0100

    qui output symfony wrapper

---
+++ lib/QUI/Package/Output.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * This file contains QUI\Package\Output
+ */
+
+namespace QUI\Package;
+
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Formatter\OutputFormatterInterface;
+
+/**
+ * Output class for the Package-Manager
+ * so he can use composer as symfony application
+ *
+ * @author www.namerobot.com (Henning Leutz)
+ */
+
+class Output extends \Symfony\Component\Console\Output\Output
+{
+    /**
+     * List of messages
+     * @var Array
+     */
+    protected $_messages = array();
+
+    /**
+     * current message string
+     * @var String
+     */
+    protected $_message  = '';
+
+    /**
+     * Writes a message to the output.
+     *
+     * @param string  $message A message to write to the output
+     * @param Boolean $newline Whether to add a newline or not
+     */
+    public function doWrite($message, $newline)
+    {
+        $this->_message .= $message;
+
+        if ( !$newline ) {
+            return;
+        }
+
+        $this->_messages[] = $this->_message;
+        $this->_message    = '';
+    }
+
+    /**
+     * Return all messages
+     * @return Array
+     */
+    public function getMessages()
+    {
+        return $this->_messages;
+    }
+
+    /**
+     * Clear all messages
+     */
+    public function clearMessages()
+    {
+        $this->_messages = array();
+    }
+}
