d58321c84ab3fbc47871bf88bccd4595df540d61
Henning Leutz <leutz@pcsg.de>
2014-07-08 12:58:18 +0200

    mail protection output

--- bin/mail_protection.php
+++ bin/mail_protection.php
@@ -15,12 +15,8 @@ header("Cache-Control: public, max-age=". 15 * 60); // 15min public caching für
 
 ?>
 <!doctype html>
-<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
-<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
-<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
-<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
 <head>
-	<title>QUIQQER Mail Protection</title>
+    <title>QUIQQER Mail Protection</title>
 </head>
-<body></body>
+<body>QUIQQER Mail Protection</body>
 </html>
