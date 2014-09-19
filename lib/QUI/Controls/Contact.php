6e457a57f4dc81b00bf31ecdb4f368eceb474cab
Henning Leutz <leutz@pcsg.de>
2014-09-19 19:33:37 +0200

    contact control

---
+++ lib/QUI/Controls/Contact.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * This file contains \QUI\Controls\Contact
+ */
+
+namespace QUI\Controls;
+
+/**
+ * Mini contact control
+ * {control control="\QUI\Controls\Contact" labels=false}
+ *
+ * @author www.pcsg.de (Henning Leutz)
+ */
+
+class Contact extends \QUI\Control
+{
+    /**
+     * constructor
+     * @param Array $attributes
+     */
+    public function __construct($attributes=array())
+    {
+        parent::setAttributes( $attributes );
+
+        $this->addCSSFile(
+            dirname( __FILE__ ) .'/Contact.css'
+        );
+
+        $this->setAttribute( 'class', 'quiqqer-contact grid-100 grid-parent' );
+        $this->setAttribute( 'labels', true );
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see \QUI\Control::create()
+     */
+    public function getBody()
+    {
+        $Engine  = \QUI::getTemplateManager()->getEngine();
+
+        $Engine->assign(array(
+            'this' => $this
+        ));
+
+        return $Engine->fetch( dirname( __FILE__ ) .'/Contact.html' );
+    }
+
+}
