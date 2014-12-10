fbe8c49ceb0013c48d01e2be9af54a609bd64185
Henning Leutz <leutz@pcsg.de>
2014-12-10 09:24:03 +0100

    neues control -> AlphabetSorting

---
+++ lib/QUI/Controls/AlphabetSorting.php
@@ -0,0 +1,101 @@
+<?php
+
+/**
+ * This file contains \QUI\Controls\AlphabetSorting
+ */
+
+namespace QUI\Controls;
+
+use QUI;
+
+/**
+ * Alphabet sorting
+ *
+ * @author www.pcsg.de (Henning Leutz)
+ */
+
+class AlphabetSorting extends QUI\Control
+{
+    /**
+     * constructor
+     * @param Array $attributes
+     */
+    public function __construct($attributes=array())
+    {
+        parent::setAttributes( $attributes );
+
+        $this->addCSSFile(
+            dirname( __FILE__ ) .'/AlphabetSorting.css'
+        );
+
+        $this->setAttribute( 'class', 'quiqqer-alphabetSorting grid-100 grid-parent' );
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see \QUI\Control::create()
+     */
+    public function getBody()
+    {
+        $Engine  = QUI::getTemplateManager()->getEngine();
+        $Site    = $this->getAttribute('Site');
+        $Project = $Site->getProject();
+
+        $active = 'abc';
+        $anchor = '';
+
+        if ( $this->getAttribute( 'anchor' ) ) {
+            $anchor = $this->getAttribute( 'anchor' );
+        }
+
+        if ( isset( $_REQUEST['list'] ) )
+        {
+            switch ( $_REQUEST['list'] )
+            {
+                case 'abc':
+                case 'def':
+                case 'ghi':
+                case 'jkl':
+                case 'mno':
+                case 'pqr':
+                case 'stu':
+                case 'vz':
+                    $active = $_REQUEST['list'];
+                break;
+            }
+        }
+
+        $attributes = $this->getAttributes();
+        $params     = array();
+
+        foreach ( $attributes as $key => $value )
+        {
+            if ( $key == 'class' ) {
+                continue;
+            }
+
+            if ( $key == 'letters' ) {
+                continue;
+            }
+
+            if ( $key == 'anchor' ) {
+                continue;
+            }
+
+
+            if ( is_string( $value ) || is_int( $value ) ) {
+                $params[ $key ] = $value;
+            }
+        }
+
+        $Engine->assign(array(
+            'active'    => $active,
+            'urlParams' => $params,
+            'anchor'    => $anchor,
+            'Site'      => $Site,
+            'Project'   => $Project
+        ));
+
+        return $Engine->fetch( dirname( __FILE__ ) .'/AlphabetSorting.html' );
+    }
+}
