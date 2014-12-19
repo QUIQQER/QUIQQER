adaa6e40460d5864d7220fabbe597d7a37ba9f12
Henning Leutz <leutz@pcsg.de>
2014-12-19 16:38:42 +0100

    breadcrumb -> control; code comment cleanup -> update class

--- lib/QUI/Update.php
+++ lib/QUI/Update.php
@@ -406,7 +406,7 @@ class Update
      * Reimportation from all menu.xml files
      * Read all packages and import the menu.xml files to the quiqqer system
      *
-     * @param Composer $Composer - optional
+     * @param \Composer\Composer $Composer - optional
      * @throws QUI\Exception
      */
     static function importAllMenuXMLs($Composer=null)
@@ -508,7 +508,7 @@ class Update
     /**
      * Reimportation from all locale.xml files
      *
-     * @param Composer $Composer - optional
+     * @param \Composer\Composer $Composer - optional
      * @throws QUI\Exception
      */
     static function importAllLocaleXMLs($Composer=null)
