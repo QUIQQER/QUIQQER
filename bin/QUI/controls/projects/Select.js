8ad5e6dca22c23456a3cccb2769f0c82585c0549
Patrick Müller <p.mueller@pcsg.de>
2015-04-01 09:27:47 +0200

    neues Control: Project Select

---
+++ bin/QUI/controls/projects/Select.js
@@ -0,0 +1,106 @@
+
+/**
+ * Dropdown for project selection
+ *
+ * @module controls/projects/Select
+ * @author www.pcsg.de (Patrick Müller)
+ *
+ * @require qui/QUI
+ * @require qui/controls/Control
+ * @require qui/controls/buttons/Select
+ * @require qui/controls/loader/Loader
+ * @require Projects
+ *
+ * @event onChange [ value ]
+ */
+
+define([
+
+    'qui/QUI',
+    'qui/controls/Control',
+    'qui/controls/buttons/Select',
+    'qui/controls/loader/Loader',
+
+    'Projects'
+
+], function(QUI, QUIControl, QUISelect, QUILoader, Projects)
+{
+    "use strict";
+
+    return new Class({
+
+        Extends : QUIControl,
+        Type    : 'controls/projects/Select',
+
+        Binds : [],
+        options : {},
+
+        initialize : function(options)
+        {
+            this.parent( options );
+
+            this.Loader = new QUILoader();
+
+            this.addEvents({
+//                onCreate : this.$onCreate
+                onIjnect : this.$onInject
+            });
+        },
+
+        /**
+         *  create
+         */
+        create : function()
+        {
+            var self = this;
+
+            this.$Elm = new Element( 'div' );
+
+            this.$Select = new QUISelect({
+                name : 'projects-select',
+                events :
+                {
+                    onChange : function(value) {
+                        self.fireEvent( 'change', [ value ] );
+                    }
+                }
+            });
+
+            this.$Select.inject( this.$Elm );
+            this.Loader.inject( this.$Elm );
+
+            this.Loader.show();
+
+            Projects.getList(function(result)
+            {
+                var i, len, langs, project;
+
+                for ( project in result )
+                {
+                    if ( !result.hasOwnProperty( project ) ) {
+                        continue;
+                    }
+
+                    langs = result[ project ].langs.split(',');
+
+                    for ( i = 0, len = langs.length; i < len; i++ )
+                    {
+                        self.$Select.appendChild(
+                            project +' ( '+ langs[ i ] +' )',
+                            project +','+ langs[ i ],
+                            'icon-home'
+                        );
+                    }
+                }
+
+                self.$Select.setValue(
+                    self.$Select.firstChild().getAttribute( 'value' )
+                );
+
+                self.Loader.hide();
+            });
+
+            return this.$Elm;
+        }
+    });
+});
