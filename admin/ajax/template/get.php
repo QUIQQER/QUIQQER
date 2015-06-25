715500d579e9ddb7b0b296df7b73c929cc60cfed
Henning Leutz <leutz@pcsg.de>
2015-06-25 16:42:18 +0200

    Template.get()
    Die Ajax Template Funtkion lässt nun Template Parameter zu

--- admin/ajax/template/get.php
+++ admin/ajax/template/get.php
@@ -3,20 +3,22 @@
 /**
  * Return a template
  *
- * @param String $template
- * @param String $package
+ * @param string $template
+ * @param string $package
+ * @param string $params
  *
- * @return String
+ * @return string
  *
  * @throws QUI\Exception
  */
 
-function ajax_template_get($template, $package)
+function ajax_template_get($template, $package, $params = '')
 {
     $Engine = QUI::getTemplateManager()->getEngine(true);
 
     if (isset($package) && !empty($package)) {
-        $template = OPT_DIR . $package .'/'. str_replace('_', '/', $template).'.html';
+        $template
+            = OPT_DIR.$package.'/'.str_replace('_', '/', $template).'.html';
     } else {
         $dir = SYS_DIR.'template/';
         $template = $dir.str_replace('_', '/', $template).'.html';
@@ -31,8 +33,13 @@ function ajax_template_get($template, $package)
         );
     }
 
+    if (!empty($params)) {
+        $params = json_decode($params, true);
+    }
+
     $Engine->assign(array(
-        'QUI' => new QUI()
+        'QUI'    => new QUI(),
+        'params' => $params
     ));
 
     return $Engine->fetch($template);
@@ -40,6 +47,6 @@ function ajax_template_get($template, $package)
 
 QUI::$Ajax->register(
     'ajax_template_get',
-    array('template', 'package'),
+    array('template', 'package', 'params'),
     'Permission::checkAdminUser'
 );
