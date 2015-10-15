5e3b5d5a75e78f4c21f929f777408dbeaba9adba
Henning Leutz <leutz@pcsg.de>
2015-10-15 08:55:37 +0200

    locale -> no_translation

--- bin/QUI/Locale.js
+++ bin/QUI/Locale.js
@@ -8,5 +8,11 @@
 define(['qui/Locale'], function (Locale) {
     "use strict";
 
+    if (window.location.search.match('lang=false')) {
+        Locale.no_translation = true;
+    } else if (window.location.toString().match('_lang_false')) {
+        Locale.no_translation = true;
+    }
+
     return Locale;
 });
