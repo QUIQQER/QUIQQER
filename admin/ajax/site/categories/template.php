5bfad7c947e2e496118156adce65a6163510aab5
Henning Leutz <leutz@pcsg.de>
2015-12-18 14:04:05 +0100

    psr2

--- admin/ajax/site/categories/template.php
+++ admin/ajax/site/categories/template.php
@@ -10,15 +10,15 @@
  */
 function ajax_site_categories_template($project, $id, $tab)
 {
-    $Project = \QUI::getProjectManager()->decode( $project );
-    $Site    = new \QUI\Projects\Site\Edit( $Project, (int)$id );
+    $Project = QUI::getProjectManager()->decode($project);
+    $Site    = new QUI\Projects\Site\Edit($Project, (int)$id);
 
-    return \QUI\Utils\String::removeLineBreaks(
-        \QUI\Utils\DOM::getTabHTML( $tab, $Site )
+    return QUI\Utils\StringHelper::removeLineBreaks(
+        QUI\Utils\DOM::getTabHTML($tab, $Site)
     );
 }
 
-\QUI::$Ajax->register(
+QUI::$Ajax->register(
     'ajax_site_categories_template',
     array('project', 'id', 'tab'),
     'Permission::checkAdminUser'
