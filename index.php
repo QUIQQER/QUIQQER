fbd290c51afc5807b525dc1330dc39b9773fd27b
Henning Leutz <leutz@pcsg.de>
2016-02-19 16:39:10 +0100

    maintenance in lib template; refresh aus error raus

--- index.php
+++ index.php
@@ -120,7 +120,7 @@ try {
             'TPL_DIR' => OPT_DIR . $Project->getName() . '/',
         ));
 
-        $file  = SYS_DIR . 'template/maintenance.html';
+        $file  = LIB_DIR . 'template/maintenance.html';
         $pfile = USR_DIR . $Project->getName() . '/lib/maintenance.html';
 
         if (file_exists($pfile)) {
