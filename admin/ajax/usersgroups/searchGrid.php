a04ef61a46999832b0c99d9c0e5e2b3500e645f9
Henning Leutz <leutz@pcsg.de>
2016-08-29 12:12:00 +0200

    feat: Benutzer und Gruppen Suche -> neue Controls

--- admin/ajax/usersgroups/searchGrid.php
+++ admin/ajax/usersgroups/searchGrid.php
@@ -66,46 +66,6 @@ QUI::$Ajax->registerFunction(
             $page,
             $limit
         );
-
-
-//        $dir    = dirname(dirname(__FILE__));
-//        $params = json_decode($params, true);
-//
-//        require_once $dir . '/users/search.php';
-//        require_once $dir . '/groups/search.php';
-//
-//        // users
-//        $users = QUI::$Ajax->callRequestFunction('ajax_users_search', array(
-//            'params' => json_encode($params)
-//        ));
-//
-//        $users = $users['result'];
-//
-//
-//        // groups
-//        if (isset($params['searchSettings'])
-//            && isset($params['searchSettings']['userSearchString'])
-//        ) {
-//            $params['search']         = $params['searchSettings']['userSearchString'];
-//            $params['searchSettings'] = array('id', 'name');
-//        }
-//
-//        $groups = QUI::$Ajax->callRequestFunction('ajax_groups_search', array(
-//            'fields' => json_encode(array()),
-//            'params' => json_encode($params)
-//        ));
-//
-//        $groups = $groups['result'];
-//
-//
-//        // combine results
-//        $result = array(
-//            'page'  => $users['page'],
-//            'total' => $users['total'] + $groups['total'],
-//            'data'  => array_merge($users['data'], $groups['data'])
-//        );
-//
-//        return $result;
     },
     array('search', 'fields', 'params'),
     'Permission::checkAdminUser'
