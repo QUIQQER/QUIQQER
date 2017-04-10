564c2fb42f53668384e2d2451604b8e53b76047d
Henning Leutz <leutz@pcsg.de>
2017-04-10 16:21:57 +0200

    feat: quiqqer/quiqqer#508

---
+++ admin/ajax/editor/get/toolbarsFromGroup.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Return the available toolbars for the user
+ *
+ * @param string / Integer $uid
+ *
+ * @return array
+ */
+QUI::$Ajax->registerFunction(
+    'ajax_editor_get_toolbarsFromGroup',
+    function ($gid, $assignedToolbars) {
+        $Group = QUI::getGroups()->get($gid);
+        $Group->setAttribute('assigned_toolbar', $assignedToolbars);
+
+        return QUI\Editor\Manager::getToolbarsFromGroup($Group);
+    },
+    array('uid', 'assignedToolbars'),
+    'Permission::checkAdminUser'
+);
