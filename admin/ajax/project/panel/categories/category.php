d41f22a344d0403dc57a17e943314f4d7d374e58
Henning Leutz <leutz@pcsg.de>
2017-04-11 10:53:10 +0200

    fix: Project Settings -> only //quiqqer/project/settings/window xml paths

---
+++ admin/ajax/project/panel/categories/category.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Return the project panel categories / tabs
+ *
+ * @param string $project - name of the project
+ * @return array
+ */
+QUI::$Ajax->registerFunction(
+    'ajax_project_panel_categories_category',
+    function ($file, $category) {
+        if (file_exists($file)) {
+            $files = array($file);
+        } else {
+            $files = json_decode($file, true);
+        }
+
+        $cacheName = 'qui/admin/menu/categories/' . md5(json_encode($files)) . '/' . $category;
+        $Settings  = QUI\Utils\XML\Settings::getInstance();
+        $Settings->setXMLPath('//quiqqer/project/settings/window');
+
+        try {
+            $result = QUI\Cache\Manager::get($cacheName);
+        } catch (QUI\Exception $Exception) {
+            try {
+                $result = $Settings->getCategoriesHtml($files, $category);
+                QUI\Cache\Manager::set($cacheName, $result);
+            } catch (\Exception $Exception) {
+                QUI\System\Log::writeException($Exception);
+                throw $Exception;
+            }
+        }
+
+        return $result;
+    },
+    array('file', 'category'),
+    'Permission::checkAdminUser'
+);
