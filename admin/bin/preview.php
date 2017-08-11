100d023ee1f3d92183eb180f0be06e59a453307c
Henning Leutz <leutz@pcsg.de>
2017-08-11 08:36:00 +0200

    fix: quiqqer/quiqqer#567 - Vorschau Funktion nicht nutzbar

--- admin/bin/preview.php
+++ admin/bin/preview.php
@@ -7,6 +7,8 @@
  */
 
 define('QUIQQER_SYSTEM', true);
+define('QUIQQER_FRONTEND', true);
+
 require_once '../header.php';
 
 if (!QUI::getUserBySession()->canUseBackend()) {
@@ -23,9 +25,15 @@ if (!isset($_POST['project']) ||
     exit;
 }
 
+$Rewrite  = QUI::getRewrite();
 $Response = QUI::getGlobalResponse();
 $Project  = QUI::getProject($_POST['project'], $_POST['lang']);
 $Site     = new QUI\Projects\Site\Edit($Project, $_POST['id']);
+
+$Rewrite->setSite($Site);
+
+$Rewrite->setPath($Site->getParents());
+$Rewrite->addSiteToPath($Site);
 
 if (isset($_POST['siteData']['type'])) {
     $Site->setAttribute('type', $_POST['siteData']['type']);
@@ -52,9 +60,14 @@ foreach ($_POST['siteDataJSON'] as $key => $value) {
 
 $Template = QUI::getTemplateManager();
 $content  = $Template->fetchSite($Site);
+$content  = QUI\Control\Manager::setCSSToHead($content);
 
 $Output  = new QUI\Output();
 $content = $Output->parse($content);
+
+$_content = $content;
+
+QUI::getEvents()->fireEvent('requestOutput', array(&$_content));
 
 $Response->headers->set("X-XSS-Protection", "0"); // <<<--- BAD
 $Response->setContent($content);
