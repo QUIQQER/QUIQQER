e6344557f37a4064360f1d6fe4958a3cf93aa765
Patrick Müller <p.mueller@pcsg.de>
2017-09-24 15:59:37 +0200

    feat: E-Mail elements select

---
+++ admin/ajax/email/validate.php
@@ -0,0 +1,18 @@
+<?php
+
+use QUI\Utils\Security\Orthos;
+
+/**
+ * Check if an email address is correct (syntax)
+ *
+ * @param string $mail - email address
+ * @return Bool
+ */
+QUI::$Ajax->registerFunction(
+    'ajax_email_validate',
+    function ($mail) {
+        return boolval(Orthos::checkMailSyntax($mail));
+    },
+    array('mail'),
+    'Permission::checkAdminUser'
+);
