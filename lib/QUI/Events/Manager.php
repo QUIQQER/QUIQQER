8aed2844456095fca9abb62402c8724e916504eb
Henning Leutz <leutz@pcsg.de>
2017-12-01 14:29:13 +0100

    refactor: Events return Wert beachtet

--- lib/QUI/Events/Manager.php
+++ lib/QUI/Events/Manager.php
@@ -83,7 +83,7 @@ class Manager implements QUI\Interfaces\Events
      */
     public static function table()
     {
-        return QUI_DB_PRFX . 'events';
+        return QUI_DB_PRFX.'events';
     }
 
     /**
@@ -115,6 +115,7 @@ class Manager implements QUI\Interfaces\Events
             QUI::getDataBase()->table()->truncate(
                 self::table()
             );
+
             return;
         }
 
@@ -276,6 +277,7 @@ class Manager implements QUI\Interfaces\Events
      * @param array|boolean $args - (optional) the argument(s) to pass to the function.
      *                          The arguments must be in an array.
      * @param boolean $force - (optional) no recursion check, optional, default = false
+     * @return array
      */
     public function fireEvent($event, $args = false, $force = false)
     {
@@ -287,6 +289,7 @@ class Manager implements QUI\Interfaces\Events
         }
 
         $this->Events->fireEvent('onFireEvent', array($event, $fireArgs));
-        $this->Events->fireEvent($event, $fireArgs, $force);
+
+        return $this->Events->fireEvent($event, $fireArgs, $force);
     }
 }
