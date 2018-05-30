182a61744956481ff564dc7db285785a460ef7fa
Henning Leutz <leutz@pcsg.de>
2018-05-30 14:04:38 +0200

    fix: Notice: Undefined index: source

--- lib/QUI/System/Console/Tools/Setup.php
+++ lib/QUI/System/Console/Tools/Setup.php
@@ -29,22 +29,27 @@ class Setup extends QUI\System\Console\Tool
      * (non-PHPdoc)
      *
      * @see \QUI\System\Console\Tool::execute()
+     * @throws QUI\Exception
      */
     public function execute()
     {
         $PackageManager = QUI::getPackageManager();
         $quiqqer        = QUI::getPackageManager()->getPackage('quiqqer/quiqqer');
+        $reference      = '';
 
+        if (isset($quiqqer['source']['reference'])) {
+            $reference = $quiqqer['source']['reference'];
+        }
 
-        $data = QUI::getLocale()->get('quiqqer/quiqqer', 'console.tool.setup.start.data', array(
+        $data = QUI::getLocale()->get('quiqqer/quiqqer', 'console.tool.setup.start.data', [
             'version'     => QUI::version(),
             'versionDate' => $PackageManager->getLastUpdateDate(),
-            'ref'         => $quiqqer['source']['reference'],
+            'ref'         => $reference,
             'date'        => QUI::getLocale()->formatDate(
                 $PackageManager->getLastUpdateCheckDate(),
                 '%B %d %Y, %X %Z'
             )
-        ));
+        ]);
 
         $data = explode('<br />', $data);
         $data = array_map(function ($entry) {
