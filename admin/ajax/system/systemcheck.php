a7fca1ecc3d7336d8ee2ba217b025d12bff1f227
Jan Wennrich <jan.wennrich@pcsg.de>
2019-07-22 15:32:43 +0200

    refactor: Systemcheck-Ajax uses Requirements-Utils for formatting now (related: 117cb0cb5e4705b4fe2adfe66e9d2525abbfb8ef)

--- admin/ajax/system/systemcheck.php
+++ admin/ajax/system/systemcheck.php
@@ -20,55 +20,7 @@ QUI::$Ajax->registerFunction(
 
         $allTests = $Requirements->getTests();
 
-        $html = '<div class="check-table">';
-
-        /** @var \QUI\Requirements\Tests\Test $Test */
-        foreach ($allTests as $category => $Tests) {
-            $html .= '<div class="system-check check-table-row">';
-            $html .= '<div class="check-table-col check-table-col-test">';
-            $html .= $category;
-            $html .= '</div>';
-            $html .= '<div class="check-table-col check-table-col-message">';
-            $html .= '<ul>';
-            foreach ($Tests as $Test) {
-                $Result           = $Test->getResult();
-                $testMessageClass = 'test-message';
-
-                // extra class for checksum
-                if ($Test->getIdentifier() == 'quiqqer.checksums') {
-                    $testMessageClass .= ' test-message-checkSum';
-                }
-
-                switch ($Result->getStatus()) {
-                    case \QUI\Requirements\TestResult::STATUS_OPTIONAL:
-                    case \QUI\Requirements\TestResult::STATUS_OK:
-                        $html .= '<li><span class="fa fa-check" title="';
-                        break;
-
-                    case \QUI\Requirements\TestResult::STATUS_FAILED:
-                        $html .= '<li class="failed"><span class="fa fa-remove" title="';
-                        break;
-
-                    case \QUI\Requirements\TestResult::STATUS_UNKNOWN:
-                    case \QUI\Requirements\TestResult::STATUS_WARNING:
-                        $html .= '<li><span class="fa fa-exclamation-circle" title="';
-                        break;
-                }
-
-                $html .= $Result->getStatusHumanReadable().'"></span>';
-                $html .= '<span class="test-name">'.$Test->getName().'</span>';
-                $html .= '<div class="'.$testMessageClass.'">';
-                $html .= $Result->getMessage();
-                $html .= '</div>';
-            }
-            $html .= '</ul>';
-            $html .= '</div>';
-            $html .= '</div>';
-        }
-
-        $html .= '</div>';
-
-        return $html;
+        return \QUI\Requirements\Utils::htmlFormatTestResults($allTests, false);
     },
     false,
     'Permission::checkSU'
