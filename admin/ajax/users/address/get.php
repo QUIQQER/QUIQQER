bd5412150d110f0e3ce88f04998e3933842de2b5
Henning Leutz <leutz@pcsg.de>
2019-10-22 14:09:00 +0200

    refactor: address get -> its now possible to get an address without user id

--- admin/ajax/users/address/get.php
+++ admin/ajax/users/address/get.php
@@ -11,11 +11,37 @@
 QUI::$Ajax->registerFunction(
     'ajax_users_address_get',
     function ($uid, $aid) {
+        if (!isset($uid) || !$uid) {
+            $result = QUI::getDataBase()->fetch([
+                'select' => ['id', 'uid'],
+                'from'   => QUI\Users\Manager::tableAddress(),
+                'where'  => [
+                    'id' => $aid
+                ],
+                'limit'  => 1
+            ]);
+
+            if (!isset($result[0])) {
+                throw new QUI\Users\Exception(
+                    QUI::getLocale()->get(
+                        'quiqqer/quiqqer',
+                        'exception.lib.user.address.not.found',
+                        [
+                            'addressId' => $aid,
+                            'userId'    => $uid
+                        ]
+                    )
+                );
+            }
+
+            $uid = (int)$result[0]['uid'];
+        }
+
         $User    = QUI::getUsers()->get((int)$uid);
         $Address = $User->getAddress((int)$aid);
 
         return $Address->getAttributes();
     },
-    array('uid', 'aid'),
+    ['uid', 'aid'],
     'Permission::checkAdminUser'
 );
--- admin/ajax/users/address/template.php
+++ admin/ajax/users/address/template.php
@@ -9,9 +9,9 @@ QUI::$Ajax->registerFunction('ajax_users_address_template', function () {
     $Engine    = QUI::getTemplateManager()->getEngine(true);
     $Countries = QUI::getCountries();
 
-    $Engine->assign(array(
+    $Engine->assign([
         'countrys' => $Countries->getList()
-    ));
+    ]);
 
-    return $Engine->fetch(SYS_DIR . 'template/users/address/edit.html');
+    return $Engine->fetch(SYS_DIR.'template/users/address/edit.html');
 });
