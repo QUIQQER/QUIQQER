1ee2b5edb43d3248bfa40f0b422fa00a7ae78235
Henning Leutz <leutz@pcsg.de>
2020-04-08 10:02:39 +0200

    fix: project creation -> template check

--- admin/ajax/project/create.php
+++ admin/ajax/project/create.php
@@ -17,7 +17,6 @@ QUI::$Ajax->registerFunction(
             $params['lang']
         );
 
-
         if (isset($params['template']) && !empty($params['template'])) {
             $Config = QUI::getProjectManager()->getConfig();
 
@@ -26,14 +25,14 @@ QUI::$Ajax->registerFunction(
             ]);
 
             $template = $params['template'];
-            $template = \QUI\Utils\Security\Orthos::removeHTML($template);
-            $template = \QUI\Utils\Security\Orthos::clearPath($template);
+            $template = QUI\Utils\Security\Orthos::removeHTML($template);
+            $template = QUI\Utils\Security\Orthos::clearPath($template);
             $Config->set($Project->getName(), 'template', $template);
             $Config->save();
         }
 
-        if (isset($params['demodata']) && $params['demodata']) {
-            \QUI\Utils\Project::applyDemoDataToProject($Project, $template);
+        if (isset($params['demodata']) && $params['demodata'] && isset($template)) {
+            QUI\Utils\Project::applyDemoDataToProject($Project, $template);
         }
 
         return $Project->getName();
