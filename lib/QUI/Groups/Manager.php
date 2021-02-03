6e78e48bd65ef1758cdcdfde1281b77d669cca81
Henning Leutz <leutz@pcsg.de>
2021-02-03 12:25:55 +0100

    fix: consider empty everony group

--- lib/QUI/Groups/Manager.php
+++ lib/QUI/Groups/Manager.php
@@ -298,7 +298,7 @@ class Manager extends QUI\QDOM
             'limit' => 1
         ]);
 
-        if (!empty($result) && ($groupId === 1 || $groupId === 0)) {
+        if (isset($result[0]) && ($groupId === 1 || $groupId === 0)) {
             $this->data[$groupId] = $result;
         }
 
