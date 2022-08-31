ee41108b758ec48f6aee598220ab887157c73026
Henning Leutz <leutz@pcsg.de>
2022-08-31 12:42:47 +0200

    feat: pcsg/projects/pocketknives-quiqqer-template#7 - external image urls

---
+++ admin/ajax/media/file/externalUpdate.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * If the image has an external image, the image will be updated
+ *
+ * @param string $project - Name of the project
+ * @param string|integer - File-ID
+ */
+
+use QUI\Projects\Media\Image;
+
+QUI::$Ajax->registerFunction(
+    'ajax_media_file_externalUpdate',
+    function ($project, $fileid) {
+        $Project = QUI\Projects\Manager::getProject($project);
+        $Media   = $Project->getMedia();
+        $File    = $Media->get((int)$fileid);
+
+        if ($File instanceof Image) {
+            $File->updateExternalImage();
+        }
+    },
+    ['project', 'fileid'],
+    'Permission::checkAdminUser'
+);
