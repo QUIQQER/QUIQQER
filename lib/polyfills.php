633deba10de877bc6eab3d23103f3b4d8441e4b7
Henning Leutz <leutz@pcsg.de>
2022-12-06 10:10:31 +0100

    fix: quiqqer/erp#88

--- lib/polyfills.php
+++ lib/polyfills.php
@@ -39,6 +39,25 @@ if (!function_exists("array_key_last")) {
 }
 
 if (!function_exists("array_merge_recursive_overwrite")) {
+    /**
+     * Merge one or more arrays recursively
+     *
+     * Merges the elements of one or more arrays together so that the values of one are appended to the end of the previous one.
+     *
+     * If the input arrays have the same string keys, then the latter value for that key will overwrite the previous one,
+     * and this is done recursively, so that if one of the values is an array itself, the function will merge it with a corresponding entry in another array too.
+     * If, however, the arrays have the same numeric key, the latter value will not overwrite the original value, but will be appended.
+     *
+     * Mimics the behaviour of array_merge_recursive() with the exception that duplicate string keys are overwritten instead of merged into an array,
+     * more akin to array_merge().
+     *
+     * @see https://www.php.net/manual/en/function.array-merge-recursive.php
+     * @see https://www.php.net/manual/en/function.array-merge.php
+     *
+     * @param array[] $arrays Variable list of arrays to recursively merge.
+     *
+     * @return array The merged array
+     */
     function array_merge_recursive_overwrite(array ...$arrays): array
     {
         $merged = [];
