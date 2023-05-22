134846e2b881c28433dd0d25770b989f62ae7a11
Henning Leutz <leutz@pcsg.de>
2023-05-22 14:33:28 +0200

    feat: composer intecration into ./console - quiqqer/quiqqer#1234

--- quiqqer.php
+++ quiqqer.php
@@ -1,28 +1,70 @@
 <?php
 
+// no console
+if (php_sapi_name() != 'cli') {
+    exit;
+}
+
+$isComposer = false;
+
+if (!empty($_SERVER['argv'])
+    && $_SERVER['argv'][0] === 'quiqqer.php'
+    && isset($_SERVER['argv'][1])
+    && $_SERVER['argv'][1] === 'composer') {
+    $isComposer = true;
+}
+
+if (!empty($_SERVER['argv'])
+    && $_SERVER['argv'][0] === './console'
+    && isset($_SERVER['argv'][1])
+    && $_SERVER['argv'][1] === 'composer') {
+    $isComposer = true;
+}
+
+if ($isComposer) {
+    unset($_SERVER['argv'][0]);
+    unset($_SERVER['argv'][1]);
+
+    $packagesDir = dirname(__FILE__, 3);
+    $cmsDir      = dirname($packagesDir);
+
+    $argv = array_values($_SERVER['argv']);
+
+    $_SERVER['argv'] = array_merge(
+        [$packagesDir . '/composer/composer/bin/composer'],
+        $argv
+    );
+
+    $_SERVER['argv'][] = '--working-dir=' . $cmsDir . '/var/composer';
+
+    require dirname(__FILE__, 3) . '/composer/composer/bin/composer';
+    exit;
+}
+
+
 /**
  * This file contains the quiqqer access for the api, cron and console
  */
-if (!\defined('CMS_DIR')) {
+if (!defined('CMS_DIR')) {
     exit;
 }
 
-\define('QUIQQER_SYSTEM', true);
-require \dirname(\dirname(\dirname(__FILE__))).'/header.php';
+define('QUIQQER_SYSTEM', true);
+require dirname(__FILE__, 3) . '/header.php';
 
 // no console
-if (\php_sapi_name() != 'cli') {
+if (php_sapi_name() != 'cli') {
     exit;
 }
 
-$conf = ETC_DIR.'conf.ini.php';
+$conf = ETC_DIR . 'conf.ini.php';
 
-if (!\file_exists($conf)) {
+if (!file_exists($conf)) {
     exit(1);
 }
 
 // Console aufbauen
-\define('QUIQQER_CONSOLE', true);
+define('QUIQQER_CONSOLE', true);
 
 $Console = new \QUI\System\Console();
 $Console->start();
