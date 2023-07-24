ca249ce04311ab1c3a7ff7f55ba9753177b617ae
Henning Leutz <leutz@pcsg.de>
2023-07-24 11:09:01 +0200

    fix: quiqqer/qsetup#166

--- admin/ajax/desktop/workspace/load.php
+++ admin/ajax/desktop/workspace/load.php
@@ -7,11 +7,14 @@ QUI::$Ajax->registerFunction(
     'ajax_desktop_workspace_load',
     function () {
         $list = QUI\Workspace\Manager::getWorkspacesByUser(QUI::getUserBySession());
+        $executed = !count(QUI\InstallationWizard\ProviderHandler::getNotSetUpProviderList());
 
-        if (!QUI::conf('mail', 'admin_mail') || QUI::conf('mail', 'admin_mail') === '') {
-            QUI::getMessagesHandler()->addError(
-                QUI::getLocale()->get('quiqqer/quiqqer', 'message.missing.admin.mail')
-            );
+        if ($executed) {
+            if (!QUI::conf('mail', 'admin_mail') || QUI::conf('mail', 'admin_mail') === '') {
+                QUI::getMessagesHandler()->addError(
+                    QUI::getLocale()->get('quiqqer/quiqqer', 'message.missing.admin.mail')
+                );
+            }
         }
 
         return $list;
