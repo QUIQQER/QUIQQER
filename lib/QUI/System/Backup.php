3e77d0314847f711c6ba6f7453fb534b2c206388
Henning Leutz <leutz@pcsg.de>
2023-09-08 12:40:32 +0200

    fix: ini backup during the cli update
    On some systems it might have happened that the ini files were restored during the update. but this
    could not be verified. to check this, an ini backup routine was written for this error during the
    update.
    
    pcsg/kunden/freemom/umsetzung#567

---
+++ lib/QUI/System/Backup.php
@@ -0,0 +1,83 @@
+<?php
+
+/**
+ * This file contains QUI\System\Backup
+ */
+
+namespace QUI\System;
+
+use QUI\Utils\System;
+use QUI\Utils\System\File;
+use QUI\Utils\Uuid;
+
+use function implode;
+use function is_dir;
+use function shell_exec;
+use function str_replace;
+
+use const ETC_DIR;
+use const VAR_DIR;
+
+class Backup
+{
+    /**
+     * @return false|string
+     */
+    public static function createEtcBackup()
+    {
+        $uuid = Uuid::get();
+        $cpIsEnabled = System::isSystemFunctionCallable('cp');
+        $backupDir = VAR_DIR . '/backup/etc/' . $uuid . '/';
+
+        File::mkdir($backupDir);
+
+        if ($cpIsEnabled === false) {
+            return false;
+        }
+
+        shell_exec('cp -r ' . ETC_DIR . '* ' . $backupDir . '.');
+
+        return $uuid;
+    }
+
+    public static function deleteEtcBackup($folder)
+    {
+        $backupFolder = VAR_DIR . 'backup/etc/' . $folder;
+
+        if (is_dir($backupFolder)) {
+            File::deleteDir($backupFolder);
+        }
+    }
+
+    public static function diff($backupFolder): string
+    {
+        $diffIsEnabled = System::isSystemFunctionCallable('diff');
+
+        if ($diffIsEnabled === false) {
+            return '';
+        }
+
+        $backupFolder = VAR_DIR . 'backup/etc/' . $backupFolder;
+
+        $params = [
+            '-ar ',
+            '--minimal ',
+            '--suppress-common-lines ',
+            '--ignore-trailing-space ',
+            "'--color=always' "
+        ];
+
+        $command = 'diff ' . implode('', $params) . ' ' . $backupFolder . ' ' . ETC_DIR;
+        $result = shell_exec($command);
+
+        $result = str_replace($backupFolder . '/', 'Old config: ', $result);
+        $result = str_replace(ETC_DIR, " - New config: ", $result);
+        $result = str_replace('\ No newline at end of file', "", $result);
+        $result = str_replace('diff', "\nFile: ", $result);
+        $result = str_replace($params, '', $result);
+        $result = str_replace("''", '', $result);
+        $result = ltrim($result);
+
+        return $result;
+    }
+}
