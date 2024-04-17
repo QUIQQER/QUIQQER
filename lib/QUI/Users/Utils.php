c09f040e25de16d6064987d06558516295d32ad4
Henning Leutz <leutz@pcsg.de>
2024-04-17 15:26:07 +0200

    refactor: js code style

--- lib/QUI/Users/Utils.php
+++ lib/QUI/Users/Utils.php
@@ -7,6 +7,7 @@
 namespace QUI\Users;
 
 use QUI;
+use QUI\Controls\Toolbar\Bar;
 use QUI\Utils\DOM;
 use QUI\Utils\Text\XML;
 
@@ -28,11 +29,11 @@ class Utils
      *
      * @param QUI\Interfaces\Users\User $User
      *
-     * @return \QUI\Controls\Toolbar\Bar
+     * @return Bar
      */
-    public static function getUserToolbar(QUI\Interfaces\Users\User $User): QUI\Controls\Toolbar\Bar
+    public static function getUserToolbar(QUI\Interfaces\Users\User $User): Bar
     {
-        $TabBar = new QUI\Controls\Toolbar\Bar([
+        $TabBar = new Bar([
             'name' => 'UserToolbar'
         ]);
 
@@ -123,7 +124,7 @@ class Utils
      *
      * @todo kick <tab> as xml in user.xml
      */
-    public static function getTab($uid, string $plugin, string $tab): string
+    public static function getTab(int|string $uid, string $plugin, string $tab): string
     {
         $Users = QUI::getUsers();
         $User = $Users->get($uid);
@@ -158,7 +159,7 @@ class Utils
 
 
         // project
-        if (strpos($plugin, 'project.') !== false) {
+        if (str_contains($plugin, 'project.')) {
             $project = explode('project.', $plugin);
 
             return DOM::getTabHTML(
