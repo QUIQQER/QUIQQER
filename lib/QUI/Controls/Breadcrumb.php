e2cfc1f461462b99012c8b78d746825f5e1b3147
Jan Wennrich <jan.wennrich@pcsg.de>
2024-05-06 12:32:05 +0200

    refactor(Controls\Breadcrumb): add parameter- & return-type declarations
    Co-authored-by: Henning Leutz <leutz@pcsg.de>

--- lib/QUI/Controls/Breadcrumb.php
+++ lib/QUI/Controls/Breadcrumb.php
@@ -20,7 +20,7 @@ class Breadcrumb extends QUI\Control
      * constructor
      * @param array $attributes
      */
-    public function __construct($attributes = [])
+    public function __construct(array $attributes = [])
     {
         // default options
         $this->setAttributes([
@@ -35,10 +35,10 @@ class Breadcrumb extends QUI\Control
     }
 
     /**
-     * (non-PHPdoc)
-     * @see \QUI\Control::create()
+     * @return string
+     * @throws QUI\Exception
      */
-    public function getBody()
+    public function getBody(): string
     {
         $Engine = QUI::getTemplateManager()->getEngine();
 
