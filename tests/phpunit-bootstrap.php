cd2ac151144c942df5c80aed52825ce0fa3cbe97
Jan Wennrich <jan.wennrich@pcsg.de>
2024-05-24 17:16:20 +0200

    chore: setup PHPUnit for project tooling

---
+++ tests/phpunit-bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+if (!defined('QUIQQER_SYSTEM')) {
+    define('QUIQQER_SYSTEM', true);
+}
+
+if (!defined('QUIQQER_AJAX')) {
+    define('QUIQQER_AJAX', true);
+}
+
+require_once __DIR__ . '/../../../../bootstrap.php';
