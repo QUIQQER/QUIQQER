4e4c0c21966009a5d689d823620eb419006a1d7e
Henning <leutz@pcsg.de>
2025-05-06 12:44:52 +0200

    feat: add cli system backup functionality
    This commit introduces a new file, `Backup.php`, under the `QUI\System\Console\Tools` namespace.
    The file contains a `Backup` class, which provides a command-line interface for creating backups of
    the whole system, the database, or the filesystem.
    
    Backup types can be specified when calling the class with the 'full', 'db', or 'filesystem'
    argument. The class automatically handles exceptions and logs helpful error messages while creating
    backups. This feature increases the reliability of the system by allowing easy creation of backups
    when needed.
    
    The `Backup` class method `createDatabaseBackup` generates database backups with the `mysqldump`
    utility, and `createFilesystemBackup` generates filesystem backups with the `tar` utility. The
    backups are saved in a dedicated backup folder.

---
+++ src/QUI/System/Console/Tools/Backup.php
@@ -0,0 +1,154 @@
+<?php
+
+/**
+ * \QUI\System\Console\Tools\Backup
+ */
+
+namespace QUI\System\Console\Tools;
+
+use QUI;
+
+/**
+ * Class Backup
+ * - Backup the complete system
+ * - Backup the database
+ * - Backup the filesystem
+ */
+class Backup extends QUI\System\Console\Tool
+{
+    /**
+     * Cleanup constructor.
+     */
+    public function __construct()
+    {
+        $this->systemTool = true;
+
+        $this->setName('quiqqer:backup')
+            ->setDescription(
+                'Create a backup of the system'
+            )
+            ->addArgument('help', 'Show this help', false, true)
+            ->addArgument('type', 'The backup type (=full|db|filesystem)', 't', true);
+    }
+
+    public function execute(): void
+    {
+        $type = $this->getArgument('type');
+
+        if (!$type) {
+            $this->outputHelp();
+            exit;
+        }
+
+        if ($type === 'full' || $type === 'db') {
+            try {
+                $this->createDatabaseBackup();
+            } catch (QUI\Exception $Exception) {
+                $this->writeLn($Exception->getMessage(), 'red');
+                $this->resetColor();
+            }
+        }
+
+        if ($type === 'full' || $type === 'filesystem') {
+            try {
+                $this->createFilesystemBackup();
+            } catch (QUI\Exception $Exception) {
+                $this->writeLn($Exception->getMessage(), 'red');
+                $this->resetColor();
+            }
+        }
+    }
+
+    /**
+     * @throws QUI\Exception
+     */
+    public function createDatabaseBackup(): void
+    {
+        $this->writeLn('Start database backup ...');
+
+        $path = VAR_DIR . 'backup/';
+        $driver = QUI::conf('db', 'driver');
+        $host = QUI::conf('db', 'host');
+        $database = QUI::conf('db', 'database');
+        $user = QUI::conf('db', 'user');
+        $password = QUI::conf('db', 'password');
+
+        if (!is_dir($path)) {
+            mkdir($path, 0777, true);
+        }
+
+        $filename = $path . 'backup_' . date('Y_m_d__H_i_s') . '.sql';
+
+        if ($driver === 'mysql') {
+            $command = sprintf(
+                'mysqldump --user=%s --password=%s --host=%s %s > %s 2>/dev/null',
+                escapeshellarg($user),
+                escapeshellarg($password),
+                escapeshellarg($host),
+                escapeshellarg($database),
+                escapeshellarg($filename)
+            );
+
+            system($command, $retVal);
+
+            if ($retVal !== 0) {
+                throw new QUI\Exception('Backup failed');
+            }
+
+            $this->writeLn('Database backup created: ' . $filename, 'green');
+            $this->writeLn('');
+            $this->resetColor();
+        } else {
+            throw new QUI\Exception('Unsupported DB driver: ' . $driver);
+        }
+    }
+
+    /**
+     * @throws QUI\Exception
+     */
+    public function createFilesystemBackup(): void
+    {
+        $this->writeLn('Start filesystem backup ...');
+
+        $path = VAR_DIR . 'backup/';
+        $filename = $path . 'backup_' . date('Y_m_d__H_i_s') . '.tar.gz';
+
+        if (!is_dir($path)) {
+            mkdir($path, 0777, true);
+        }
+
+        $base = rtrim(CMS_DIR, '/');
+        $folders = ['etc', 'media', 'packages', 'usr', 'var'];
+        $include = [];
+
+        foreach ($folders as $folder) {
+            $include[] = escapeshellarg($folder);
+        }
+
+        // Exclude certain subfolders in var
+        $exclude = [
+            '--exclude=var/cache',
+            '--exclude=var/tmp',
+            '--exclude=var/uploads',
+            '--exclude=var/sessions',
+            '--exclude=var/backup'
+        ];
+
+        $command = sprintf(
+            'cd %s && tar czf %s %s %s',
+            escapeshellarg($base),
+            escapeshellarg($filename),
+            implode(' ', $exclude),
+            implode(' ', $include)
+        );
+
+        system($command, $retVal);
+
+        if ($retVal !== 0) {
+            throw new QUI\Exception('Filesystem backup failed');
+        }
+
+        $this->writeLn('Filesystem backup created: ' . $filename, 'green');
+        $this->resetColor();
+    }
+}
