44a5a50097d3387133d1fc7d4a0733fdeac7281a
Henning <leutz@pcsg.de>
2026-02-13 10:51:16 +0100

    fix: remove cache test in header.php
    In this commit, the initial cache test in `src/header.php` has been removed. This test was trying
    to set 'qui_cache_test' to 1 and checking it afterwards. The try-catch block catching
    QUI\\Cache\\Exception and Stash\\Exception\\InvalidArgumentException has also been eliminated. This
    simplifies the file making it cleaner and more straightforward.
    
    Constant is not used anymore
    
    Related: quiqqer/core#1469

--- src/header.php
+++ src/header.php
@@ -81,14 +81,6 @@ define('MAIL_PROTECT', QUI::conf('globals', 'mailprotection'));
 define('ADMIN_CACHE', false);
 define('DEBUG_MEMORY', false);
 
-// Cacheflag setzen
-try {
-    QUI\Cache\Manager::set('qui_cache_test', 1);
-    define('CHECK_CACHE', QUI\Cache\Manager::get('qui_cache_test'));
-} catch (QUI\Cache\Exception | Stash\Exception\InvalidArgumentException) {
-    define('CHECK_CACHE', false);
-}
-
 $error_mail = QUI::conf('error', 'mail');
 
 if (!empty($error_mail)) {
