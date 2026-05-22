df9c3e6075eee7230ae105d3272c82b44b923a30
Henning <leutz@pcsg.de>
2026-05-22 12:49:28 +0200

    fix: handle exceptions in MigrationV2 and autoload
    This commit ensures the MigrationV2 script handles exceptions instead of breaking when an error
    occurs. It catches any exceptions thrown, checks if they are not 404 errors, and displays the error
    message without terminating the process.
    
    Autoload now has conditionals that check if the PHP interface is CLI, and if so, it echoes the
    error message, the error code, and some additional details, but without affecting the HTTP response
    for other clients. The error message is then written to the error log for future reference.
    
    Additionally, it mitigates against trying to set http_response_code or header in CLI mode and after
    headers have been sent in web mode. This is especially significant for avoiding 'headers already
    sent' errors.

--- src/autoload.php
+++ src/autoload.php
@@ -131,7 +131,7 @@ function exception_handler(\Throwable $Exception): void
 {
     $code = $Exception->getCode();
 
-    if ($code >= 400 && $code < 600) {
+    if (php_sapi_name() !== 'cli' && !headers_sent() && $code >= 400 && $code < 600) {
         http_response_code($code);
         header('Content-Type: application/json');
     }
@@ -144,6 +144,21 @@ function exception_handler(\Throwable $Exception): void
 
     if (!$isCacheMiss) {
         Log::addError($Exception->getMessage());
+    }
+
+    if (php_sapi_name() === 'cli') {
+        echo PHP_EOL;
+        echo 'Error: ' . $Exception->getMessage() . PHP_EOL;
+
+        if ($code) {
+            echo 'Code: ' . $code . PHP_EOL;
+        }
+
+        if (!$isCacheMiss) {
+            echo 'Details were written to the error log.' . PHP_EOL;
+        }
+
+        return;
     }
 
     echo json_encode([
