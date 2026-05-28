7f19dd6c9c869542c8f00a7941aae9181cd7caae
Michael Danielczok <michael@pcsg.de>
2026-05-28 16:27:45 +0200

    feat(Breadcrumb): modernize slider behavior and add display options
    Extend the breadcrumb control with configurable title visibility,
    custom intro text, font size, separators and last-item highlighting.

--- bin/Controls/BreadcrumbSlider.js
+++ bin/Controls/BreadcrumbSlider.js
@@ -17,329 +17,226 @@ define('package/quiqqer/core/bin/Controls/BreadcrumbSlider', [
     return new Class({
 
         Extends: QUIControl,
-        Type   : 'package/quiqqer/core/bin/Controls/BreadcrumbDropDown',
+        Type   : 'package/quiqqer/core/bin/Controls/BreadcrumbSlider',
 
         Binds: [
             '$onImport',
+            '$onResize',
             '$onScroll',
-            'prev',
-            'next',
-            'resize'
+            '$refresh',
+            '$alignToEnd',
+            '$updateMask',
+            '$onMouseDown',
+            '$onMouseMove',
+            '$onMouseUp',
+            '$onDragStart',
+            '$onClickCapture'
         ],
 
         initialize: function (options)
         {
             this.parent(options);
 
-            this.$SlideFX = null;
-            this.$Prev = null;
-            this.$Next = null;
-            this.$Inner = null;
-            this.$InnerScrollSize = null;
-            this.$InnerSize = null;
-            this.$IconWidth = 0;
-
-            this.$scrollMax = 0;
-
-            this.nextButton = false;
-            this.prevButton = false;
+            this.$Container = null;
+            this.$supportsDragNavigation = false;
+            this.$tolerance = 1;
+            this.$dragThreshold = 6;
+            this.$isMouseDown = false;
+            this.$isDragging = false;
+            this.$preventClick = false;
+            this.$startX = 0;
+            this.$startScrollLeft = 0;
+            this.$resizeHandler = QUIFunctionUtils.debounce(this.$onResize, 100);
 
             this.addEvents({
                 onImport: this.$onImport
             });
 
-            QUI.addEvent('resize', this.resize);
+            QUI.addEvent('resize', this.$resizeHandler);
+            QUI.addEvent('onResize', this.$resizeHandler);
         },
 
-        /**
-         * resize the control and recalc the $Inner width
-         */
-        resize: function ()
-        {
-            var offset = 0;
-
-            if ((this.prevButton === true) && (this.nextButton === true)) {
-                offset = this.$IconWidth * 2;
-            } else {
-                if (this.prevButton === true || this.nextButton === true) {
-                    offset = this.$IconWidth;
-                }
-            }
-
-            moofx(this.$Inner).animate({
-                width: document.getElement('.quiqqer-breadcrumb-wrapper').getSize().x - offset
-            }, {
-                duration: 300,
-                callback: function ()
-                {
-                    this.$InnerSize = this.$Inner.getSize().x + offset;
-                    this.$onScroll();
-                }.bind(this)
-            });
-        },
-
-        /**
-         * event : on import
-         */
         $onImport: function ()
         {
-            var Elm  = this.getElm(),
-                size = Elm.getSize();
+            this.$Container = this.getElm().getElement('.quiqqer-breadcrumb-container');
 
-            // next button
-            this.$Next = new Element('div', {
-                'class': 'quiqqer-breadcrumb-slider-next',
-                html   : '<span class="fa fa-arrow-circle-right"></span>',
-                styles : {
-                    display   : 'none',
-                    lineHeight: size.y,
-                    opacity   : 0,
-                    right     : -this.$IconWidth
-                },
-                events : {
-                    click: this.next
-                }
-            }).inject(Elm.getElement('.quiqqer-breadcrumb-wrapper'));
-
-            // prev button
-            this.$Prev = new Element('div', {
-                'class': 'quiqqer-breadcrumb-slider-prev',
-                html   : '<span class="fa fa-arrow-circle-left"></span>',
-                styles : {
-                    display   : 'none',
-                    left      : 0,
-                    lineHeight: size.y,
-                    opacity   : 0
-                },
-                events : {
-                    click: this.prev
-                }
-            }).inject(Elm.getElement('.quiqqer-breadcrumb-wrapper'));
-
-
-            this.$IconWidth = Elm.getElement('.quiqqer-breadcrumb-slider-next').getDimensions().x;
-            this.$Inner = Elm.getElement('.quiqqer-breadcrumb-container');
-            this.$InnerSize = this.$Inner.getSize().x;
-            this.$InnerScrollSize = this.$Inner.getScrollSize().x;
-
-            this.$SlideFX = new Fx.Scroll(this.$Inner);
-
-            var scrollSpy = QUIFunctionUtils.debounce(this.$onScroll, 200);
-
-            this.$Inner.addEvent('scroll', scrollSpy);
-
-            this.$NextFX = moofx(this.$Next);
-            this.$PrevFX = moofx(this.$Prev);
-
-            if (this.$InnerScrollSize > this.$InnerSize) {
-                this.showNextButton.delay(200, this);
+            if (!this.$Container) {
+                return;
             }
+
+            this.$supportsDragNavigation = this.$canUseDragNavigation();
+
+            this.$Container.addEvent(
+                'scroll',
+                QUIFunctionUtils.debounce(this.$onScroll, 30)
+            );
+
+            this.$Container.addEvent('click', this.$onClickCapture);
+
+            if (this.$supportsDragNavigation) {
+                this.$Container.addEvent('mousedown', this.$onMouseDown);
+                this.$Container.addEvent('dragstart', this.$onDragStart);
+                document.addEventListener('mousemove', this.$onMouseMove, {
+                    passive: false
+                });
+                document.addEventListener('mouseup', this.$onMouseUp);
+                window.addEventListener('blur', this.$onMouseUp);
+                this.$Container.addClass('quiqqer-breadcrumb-container--draggable');
+            }
+
+            this.$alignToEnd();
+            this.$refresh();
         },
 
-        /**
-         * scroll to preview
-         *
-         * @return {Promise}
-         */
-        prev: function ()
+        $onResize: function ()
         {
-            return new Promise(function (resolve)
-            {
-                var left = this.$Inner.getScroll().x - this.$InnerSize;
+            if (!this.$Container) {
+                return;
+            }
 
-                if (left < 0) {
-                    left = 0;
-                }
-
-                this.$SlideFX.start(left, 0).chain(resolve);
-
-            }.bind(this));
+            this.$alignToEnd();
+            this.$refresh();
         },
 
-        /**
-         * scroll to next
-         *
-         * @return {Promise}
-         */
-        next: function ()
-        {
-            return new Promise(function (resolve)
-            {
-                var left = this.$Inner.getScroll().x + this.$InnerSize;
-                this.$SlideFX.start(left, 0).chain(resolve);
-            }.bind(this));
-        },
-
-        /**
-         * Show the next button
-         * @returns {Promise}
-         */
-        showNextButton: function ()
-        {
-            return new Promise(function (resolve)
-            {
-                if (this.nextButton) {
-                    return;
-                }
-
-                this.nextButton = true;
-                this.$Next.setStyle('display', null);
-
-                var right   = 0,
-                    opacity = 1,
-                    offset  = this.$IconWidth;
-
-                if (this.prevButton === true) {
-                    offset = this.$IconWidth * 2;
-                }
-
-                moofx(this.$Inner).animate({
-                    width: this.$InnerSize - offset
-                }, {
-                    duration: 300
-                });
-
-                this.$NextFX.animate({
-                    right  : right,
-                    opacity: opacity
-                }, {
-                    duration: 200,
-                    callback: resolve
-                });
-            }.bind(this));
-        },
-
-        /**
-         * Show the previous button
-         * @returns {Promise}
-         */
-        showPrevButton: function ()
-        {
-            return new Promise(function (resolve)
-            {
-                if (this.prevButton) {
-                    return;
-                }
-
-                this.prevButton = true;
-                this.$Prev.setStyle('display', null);
-
-                var left    = 0,
-                    opacity = 1,
-                    offset  = this.$IconWidth;
-
-                if (this.nextButton === true) {
-                    offset = this.$IconWidth * 2;
-                }
-
-                moofx(this.$Inner).animate({
-                    left : this.$IconWidth,
-                    width: this.$InnerSize - offset
-                }, {
-                    duration: 300
-                });
-
-                this.$PrevFX.animate({
-                    left   : left,
-                    opacity: opacity
-                }, {
-                    duration: 300,
-                    callback: resolve
-                });
-            }.bind(this));
-        },
-
-        /**
-         * Hide the next button
-         * @returns {Promise}
-         */
-        hideNextButton: function ()
-        {
-            return new Promise(function (resolve)
-            {
-                if (!this.nextButton) {
-                    return;
-                }
-
-                this.nextButton = false;
-
-                this.$NextFX.animate({
-                    right  : -this.$IconWidth,
-                    opacity: 0
-                }, {
-                    duration: 300,
-                    callback: function ()
-                    {
-                        this.$Next.setStyle('display', 'none');
-
-                        resolve();
-                    }.bind(this)
-                });
-                
-                moofx(this.$Inner).animate({
-                    width: this.$InnerSize - this.$IconWidth
-                }, {
-                    duration: 300
-                });
-
-            }.bind(this));
-        },
-
-        /**
-         * Hide the prev button
-         * @returns {Promise}
-         */
-        hidePrevButton: function ()
-        {
-            return new Promise(function (resolve)
-            {
-                if (!this.prevButton) {
-                    return;
-                }
-
-                this.prevButton = false;
-
-                this.$PrevFX.animate({
-                    left   : 0,
-                    opacity: 0
-                }, {
-                    duration: 200,
-                    callback: function ()
-                    {
-                        this.$Prev.setStyle('display', 'none');
-                        resolve();
-                    }.bind(this)
-                });
-
-                moofx(this.$Inner).animate({
-                    left : 0,
-                    width: this.$Inner.getSize().x + this.$IconWidth
-                }, {
-                    duration: 300
-                });
-
-            }.bind(this));
-        },
-
-        /**
-         * event : on scroll
-         * look for the prev and next button
-         */
         $onScroll: function ()
         {
-            var left = this.$Inner.getScroll().x;
+            this.$refresh();
+        },
 
-            if (left === 0) {
-                this.hidePrevButton();
-            } else {
-                this.showPrevButton();
+        $canUseDragNavigation: function ()
+        {
+            if (!window.matchMedia) {
+                return true;
             }
 
-            if (left === (this.$Inner.getScrollSize().x - this.$Inner.getSize().x)) {
-                this.hideNextButton();
-            } else {
-                this.showNextButton();
+            return window.matchMedia('(hover: hover) and (pointer: fine)').matches;
+        },
+
+        $hasOverflow: function ()
+        {
+            return this.$Container.scrollWidth > this.$Container.clientWidth + this.$tolerance;
+        },
+
+        $getMaxScrollLeft: function ()
+        {
+            return Math.max(
+                this.$Container.scrollWidth - this.$Container.clientWidth,
+                0
+            );
+        },
+
+        $alignToEnd: function ()
+        {
+            if (!this.$hasOverflow()) {
+                this.$Container.scrollLeft = 0;
+                return;
             }
+
+            this.$Container.scrollLeft = this.$getMaxScrollLeft();
+        },
+
+        $refresh: function ()
+        {
+            this.$updateMask();
+        },
+
+        $updateMask: function ()
+        {
+            var hasOverflow, isAtStart, isAtEnd;
+
+            if (!this.$Container) {
+                return;
+            }
+
+            hasOverflow = this.$hasOverflow();
+            isAtStart = this.$Container.scrollLeft <= this.$tolerance;
+            isAtEnd = this.$Container.scrollLeft + this.$Container.clientWidth >=
+                this.$Container.scrollWidth - this.$tolerance;
+
+            this.$Container.style.setProperty(
+                '--qui-breadcrumb-mask-start',
+                hasOverflow && !isAtStart ? 'var(--qui-breadcrumb-mask-size)' : '0px'
+            );
+
+            this.$Container.style.setProperty(
+                '--qui-breadcrumb-mask-end',
+                hasOverflow && !isAtEnd ? 'var(--qui-breadcrumb-mask-size)' : '0px'
+            );
+        },
+
+        $onMouseDown: function (event)
+        {
+            if (!this.$hasOverflow() || event.rightClick || event.which === 3) {
+                return;
+            }
+
+            event.preventDefault();
+            this.$isMouseDown = true;
+            this.$isDragging = false;
+            this.$preventClick = false;
+            this.$startX = event.page.x;
+            this.$startScrollLeft = this.$Container.scrollLeft;
+        },
+
+        $onMouseMove: function (event)
+        {
+            var deltaX;
+
+            if (!this.$isMouseDown) {
+                return;
+            }
+
+            if (typeof event.buttons !== 'undefined' && event.buttons !== 1) {
+                this.$onMouseUp();
+                return;
+            }
+
+            deltaX = event.pageX - this.$startX;
+
+            if (!this.$isDragging && Math.abs(deltaX) < this.$dragThreshold) {
+                return;
+            }
+
+            if (!this.$isDragging) {
+                this.$isDragging = true;
+                this.$preventClick = true;
+                this.$Container.addClass('is-dragging');
+            }
+
+            event.preventDefault();
+            this.$Container.scrollLeft = this.$startScrollLeft - deltaX;
+            this.$refresh();
+        },
+
+        $onMouseUp: function ()
+        {
+            this.$isMouseDown = false;
+            this.$Container.removeClass('is-dragging');
+
+            if (!this.$isDragging) {
+                return;
+            }
+
+            this.$isDragging = false;
+
+            (function ()
+            {
+                this.$preventClick = false;
+            }).delay(0, this);
+        },
+
+        $onDragStart: function (event)
+        {
+            event.preventDefault();
+        },
+
+        $onClickCapture: function (event)
+        {
+            if (!this.$preventClick) {
+                return;
+            }
+
+            event.stop();
         }
     });
 });
--- src/QUI/Controls/Breadcrumb.php
+++ src/QUI/Controls/Breadcrumb.php
@@ -13,13 +13,35 @@ use QUI;
  */
 class Breadcrumb extends QUI\Control
 {
+    protected array $allowedSeparators = [
+        'angle-right',
+        'chevron-right',
+        'angles-right',
+        'caret-right',
+        'slash',
+        'bullet',
+        'pipe'
+    ];
+
+    protected array $allowedLastItemStyles = [
+        'none',
+        'primary',
+        'bold',
+        'primary-bold'
+    ];
+
     public function __construct(array $attributes = [])
     {
         // default options
         $this->setAttributes([
             'class' => 'quiqqer-breadcrumb',
             'controlHeight' => 40,
-            'layout' => 'slider'
+            'layout' => 'slider',
+            'showTitle' => true,
+            'titleText' => '',
+            'fontSize' => '0.9em',
+            'separator' => 'angle-right',
+            'lastItemStyle' => 'primary'
         ]);
 
         parent::__construct($attributes);
@@ -33,10 +55,30 @@ class Breadcrumb extends QUI\Control
     public function getBody(): string
     {
         $Engine = QUI::getTemplateManager()->getEngine();
+        $separator = $this->normalizeSeparator(
+            (string)$this->getAttribute('separator')
+        );
+        $lastItemStyle = $this->normalizeLastItemStyle(
+            (string)$this->getAttribute('lastItemStyle')
+        );
+        $fontSize = $this->normalizeFontSize(
+            (string)$this->getAttribute('fontSize')
+        );
+
+        $this->setAttribute('separator', $separator);
+        $this->setAttribute('lastItemStyle', $lastItemStyle);
+        $this->setAttribute('fontSize', $fontSize);
+        $this->setAttribute(
+            'showTitle',
+            (int)(bool)$this->getAttribute('showTitle')
+        );
+
+        $separatorConfig = $this->getSeparatorConfig($separator);
 
         $Engine->assign([
             'this' => $this,
-            'Rewrite' => QUI::getRewrite()
+            'Rewrite' => QUI::getRewrite(),
+            'separatorConfig' => $separatorConfig
         ]);
 
         $this->setAttribute(
@@ -45,6 +87,11 @@ class Breadcrumb extends QUI\Control
         );
 
         $this->setStyle('height', $this->getAttribute('controlHeight'));
+        $this->setStyle('font-size', $fontSize);
+        $this->setAttribute('data-qui-breadcrumb-separator', $separator);
+        $this->setAttribute('data-qui-breadcrumb-last-item-style', $lastItemStyle);
+        $this->addCSSClass('quiqqer-breadcrumb--separator-' . $separator);
+        $this->addCSSClass('quiqqer-breadcrumb--last-item-' . $lastItemStyle);
 
         $layout = strtolower($this->getAttribute('layout'));
 
@@ -74,5 +121,87 @@ class Breadcrumb extends QUI\Control
         $this->addCSSFile(__DIR__ . $css);
 
         return $Engine->fetch(__DIR__ . $template);
+    }
+
+    protected function normalizeSeparator(string $separator): string
+    {
+        if (!in_array($separator, $this->allowedSeparators, true)) {
+            return 'angle-right';
+        }
+
+        return $separator;
+    }
+
+    protected function normalizeLastItemStyle(string $lastItemStyle): string
+    {
+        if (!in_array($lastItemStyle, $this->allowedLastItemStyles, true)) {
+            return 'none';
+        }
+
+        return $lastItemStyle;
+    }
+
+    protected function normalizeFontSize(string $fontSize): string
+    {
+        if (preg_match('/^\d+(?:\.\d+)?(?:em|rem|px|%)$/', $fontSize)) {
+            return $fontSize;
+        }
+
+        return '0.9em';
+    }
+
+    protected function getSeparatorConfig(string $separator): array
+    {
+        switch ($separator) {
+            case 'chevron-right':
+                return [
+                    'type' => 'icon',
+                    'class' => 'fa-chevron-right',
+                    'text' => ''
+                ];
+
+            case 'angles-right':
+                return [
+                    'type' => 'icon',
+                    'class' => 'fa-angles-right',
+                    'text' => ''
+                ];
+
+            case 'caret-right':
+                return [
+                    'type' => 'icon',
+                    'class' => 'fa-caret-right',
+                    'text' => ''
+                ];
+
+            case 'slash':
+                return [
+                    'type' => 'text',
+                    'class' => '',
+                    'text' => '/'
+                ];
+
+            case 'bullet':
+                return [
+                    'type' => 'text',
+                    'class' => '',
+                    'text' => '•'
+                ];
+
+            case 'pipe':
+                return [
+                    'type' => 'text',
+                    'class' => '',
+                    'text' => '|'
+                ];
+
+            case 'angle-right':
+            default:
+                return [
+                    'type' => 'icon',
+                    'class' => 'fa-angle-right',
+                    'text' => ''
+                ];
+        }
     }
 }
