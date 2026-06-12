7195b5f368a267db2d1010e9552b4184a371bb98
Henning <leutz@pcsg.de>
2026-06-12 17:36:57 +0200

    refactor: improve database handling in admin scripts
    This commit refactors how two admin ajax scripts handle database interactions for improved
    efficiency and security.
    
    - In `admin/ajax/search/lastEditSites.php`, the PDO object has been replaced with a Doctrine DBAL
    connection, and the SQL query has been constructed using Doctrine's query builder for better
    security.
    - The code is now more robust, checking if the projects array is empty before running the database
    queries.
    - Indexes, as well as project and language parameters, are used to enhance code efficiency.
    
    - In `admin/ajax/system/optimize.php`, the script now checks if the database platform is a MySQL
    platform before attempting to optimize tables: this prevents errors on incompatible platforms.
    - The optimization process has been improved by iterating over each table in the schema and
    optimizing them individually.
    
    Overall, this commit improves the stability, efficiency and security of the affected admin scripts.
    
    Related: quiqqer/core#1525

--- admin/ajax/system/optimize.php
+++ admin/ajax/system/optimize.php
@@ -7,8 +7,18 @@
 QUI::$Ajax->registerFunction(
     'ajax_system_optimize',
     static function (): void {
-        $Table = QUI::getDataBase()->table();
-        $Table->optimize($Table->getTables());
+        $Connection = QUI::getDataBaseConnection();
+        $Platform = $Connection->getDatabasePlatform();
+
+        if (!($Platform instanceof \Doctrine\DBAL\Platforms\AbstractMySQLPlatform)) {
+            return;
+        }
+
+        foreach (QUI::getSchemaManager()->listTableNames() as $table) {
+            $Connection->executeStatement(
+                'OPTIMIZE TABLE ' . $Platform->quoteSingleIdentifier($table)
+            );
+        }
     },
     false,
     [
