a498c31b9064e05601cd43334c09fc23a1fc2def
Henning <leutz@pcsg.de>
2026-09-02 08:54:42 +0200

    fix(settings): protect core secrets from aliased paths
    Related: quiqqer/core#1549

---
+++ tests/unit/QUI/SettingsAjaxSecurityTest.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace QUI;
+
+use PHPUnit\Framework\Attributes\PreserveGlobalState;
+use PHPUnit\Framework\Attributes\RunTestsInSeparateProcesses;
+use PHPUnit\Framework\TestCase;
+
+#[RunTestsInSeparateProcesses]
+#[PreserveGlobalState(false)]
+final class SettingsAjaxSecurityTest extends TestCase
+{
+    public function testAliasedCoreConfigPathKeepsSensitiveSettingsHidden(): void
+    {
+        \QUI::$Ajax = new Ajax();
+
+        $coreDirectory = dirname(__DIR__, 3);
+        require $coreDirectory . '/admin/ajax/settings/get.php';
+
+        $callable = Ajax::getRegisteredCallables()['ajax_settings_get']['callable'];
+        $aliasedConfigFile = $coreDirectory . '/admin/settings/../settings/conf.xml';
+
+        self::assertStringNotContainsString(
+            'quiqqer/core/admin/settings/conf.xml',
+            $aliasedConfigFile
+        );
+        self::assertSame(
+            realpath($coreDirectory . '/admin/settings/conf.xml'),
+            realpath($aliasedConfigFile)
+        );
+
+        $config = $callable(json_encode($aliasedConfigFile, JSON_THROW_ON_ERROR));
+
+        self::assertArrayHasKey('globals', $config);
+        self::assertArrayNotHasKey('db', $config);
+        self::assertArrayNotHasKey('openssl', $config);
+        self::assertArrayNotHasKey('salt', $config['globals']);
+        self::assertArrayNotHasKey('saltlength', $config['globals']);
+        self::assertArrayNotHasKey('cms_dir', $config['globals']);
+        self::assertArrayNotHasKey('var_dir', $config['globals']);
+        self::assertArrayNotHasKey('usr_dir', $config['globals']);
+        self::assertArrayNotHasKey('opt_dir', $config['globals']);
+        self::assertArrayNotHasKey('rootuser', $config['globals']);
+        self::assertArrayNotHasKey('root', $config['globals']);
+    }
+}
